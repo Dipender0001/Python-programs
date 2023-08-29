ca1e1c522684e73e003be955cb3df1fcdece4cd9
Dipender0001 <143435488+Dipender0001@users.noreply.github.com>
2023-08-29 22:27:17 +0530

    Add files via upload

---
+++ PEERIMETER AND AREA OF RECTANGLE.py
@@ -0,0 +1,8 @@
+#program to find area of rectangle
+l = float(input("enter the length of rectangle:"))
+b = float(input("enter the breadth of rectangle"))
+peri = 2*(l+b)
+area = l*b
+print("area of rectangle is:",area)
+print("perimeter of rectangle is:",peri)
+
