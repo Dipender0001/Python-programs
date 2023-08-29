39d60173b01b8b0ddb1be59a78e4e9a51ab2b013
Dipender0001 <143435488+Dipender0001@users.noreply.github.com>
2023-08-29 22:27:54 +0530

    Add files via upload

---
+++ AREA OF CIRCLE.py
@@ -0,0 +1,5 @@
+#program to find area of rectangle
+l=(float(input("enter the length of rectangle:"))
+b = (float(input("enter the breadth of rectangle"))
+area = 2*(l+b)
+print("area of rectangle is:",area)
