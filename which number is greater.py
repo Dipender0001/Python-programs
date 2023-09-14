66066e76e3d8cb54789045b9be1d7a6b238208a7
Dipender0001 <143435488+Dipender0001@users.noreply.github.com>
2023-09-14 22:40:37 +0530

    Add files via upload

---
+++ which number is greater.py
@@ -0,0 +1,8 @@
+# check which number is greater between the 2 numbers 
+a = eval(input("enter the first number:"))
+b = eval(input("enter the second number:"))
+
+if a>b:
+    print("first number is greater then second")
+else:
+    print("second number is greater than first")
