16b1916af2688c4230096748b4cff868bf503efd
Dipender0001 <143435488+Dipender0001@users.noreply.github.com>
2023-09-14 22:32:52 +0530

    Add files via upload

---
+++ QUES 4.py
@@ -0,0 +1,7 @@
+#check whether the candidate is eligible to vote or not
+age = int(input("enter the age of the candidate"))
+
+if age>= 18:
+    print("eligible to vote:")
+else:
+     print("not eligible to vote")
